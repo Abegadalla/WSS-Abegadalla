752cdbd5531fa370c9d1727eb7695c9acd9cc701
Paul Abbott <abbott@wolfram.com>
2019-07-03 15:40:59 -0400

    Created Table of Contents for Angles Directory

--- Computational Thinking/AGProject.m
+++
@@ -1,3 +0,0 @@
-{"Name" -> "Abe Gadalla", "Directory" -> "/Users/paul/Documents/Projects/Wolf\
-ram Research/Wolfram Summer School/WSS19/Git/Abe Gadalla/Computational \
-Thinking/", "Files" -> {"WSS 2019 Geometry.nb"}}
---
+++ Computational Thinking/Geometry/Angles/Angles.m
@@ -0,0 +1,7 @@
+{"Name" -> "Geometry \[RightTriangle] Angles", 
+ "Directory" -> "/Users/paul/Documents/Projects/Wolfram Research/Wolfram \
+Summer School/WSS19/Git/Abe Gadalla/Computational Thinking/Geometry/Angles/", 
+ "Files" -> {"Ratio between an angle and its complement 1.nb", 
+   "Ratio between an angle and its complement 2.nb", 
+   "Ratio between an angle and its supplementary 1.nb", 
+   "Ratio between an angle and its supplement.nb"}}
