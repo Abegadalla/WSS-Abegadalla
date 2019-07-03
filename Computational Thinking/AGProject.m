25d59fdfe943a79cc2cc28f947548160e5acf219
Paul Abbott <abbott@wolfram.com>
2019-07-03 15:11:02 -0400

    Defining a project to add a Table of Contents

---
+++ Computational Thinking/AGProject.m
@@ -0,0 +1,3 @@
+{"Name" -> "Abe Gadalla", "Directory" -> "/Users/paul/Documents/Projects/Wolf\
+ram Research/Wolfram Summer School/WSS19/Git/Abe Gadalla/Computational \
+Thinking/", "Files" -> {"WSS 2019 Geometry.nb"}}
