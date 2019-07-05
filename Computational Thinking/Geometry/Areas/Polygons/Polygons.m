7318c951810c7d5d2439204f07348d9a5e30e63f
Abraham Gadalla <abegadalla@yahoo.com>
2019-07-04 20:59:57 -0400

    Construct table of contents for Notebook directories

---
+++ Computational Thinking/Geometry/Areas/Polygons/Polygons.m
@@ -0,0 +1,10 @@
+{"Name" -> "Areas of Polygons", "Directory" -> "/Users/a_gadalla/Documents/Gi\
+tHub/WSS-Template/Computational Thinking/Geometry/Areas/Polygons/", 
+ "Files" -> {"A sqr within square solution 1.nb", 
+   "Calculate the ratio of shaded Areas in a Rectangle.nb", 
+   "Four Flower Patel 7-3-19.nb", "Ratio between areas of two squares.nb", 
+   "Rectangle in a circle.nb", "rectangle in a cyclical trapezoid.nb", 
+   "rectangle in cyclical trapezoid.nb", 
+   "Rotating a rectangle about square's center.nb", 
+   "shaded area in a square 7 - 3.nb", "Shaded area in a square II.nb", 
+   "Trapezoid in semicircle.nb", "Triangle in a square.nb"}}
---
+++ Computational Thinking/Geometry/Areas/Triangles/Triangles.m
@@ -0,0 +1,7 @@
+{"Name" -> "Areas of Triangles", "Directory" -> "/Users/a_gadalla/Documents/G\
+itHub/WSS-Template/Computational Thinking/Geometry/Areas/Triangles/", 
+ "Files" -> {"Area of a triangle in a polygon 5-26-19.nb", 
+   "Area of a triangle in a polygon 6-24-19.nb", 
+   "Area of a triangle within a polygon.nb", 
+   "Area of parts of isosceles right triangle.nb", 
+   "Equilateral Triangles.nb", "Ratios of areas of Triangles.nb"}}
